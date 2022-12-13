e9b769920b8df94f8e428d1172c10613e043e729
Muriithi Ian <imuriithiian@gmail.com>
2022-12-13 14:50:40 +0300

    completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+        def create
+          user = User.find_by(username: params[:username])
+          if user
+            session[:user_id] ||= user.id
+            render json: user, status: 200
+          else
+            render json: { error: "User Not Found!" }
+          end
+        end
+    
+        def destroy
+          session.delete :user_id
+          head :no_content
+        end
+end
